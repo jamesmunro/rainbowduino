7b8b09177e9f8024b1d44efb34d1e07f2bac6d50
Icing.Seeed <Icing.Seeed@81d1cde0-2722-c43a-a157-55f8df228ea3>
2010-06-02 01:37:52 +0000

    Initial Version for controlling the Rainbowduino. There are two folders: 1. Rainbow_commandVersion_Beta is controlling in command way through I2C. 2. Rainbowduino_v1_0_4 is  just an demo on how to control the rainbow Matrix and add an serial interface

---
+++ trunk/Rainbowduino_v1_0_4/Rainbow.cpp
@@ -0,0 +1,580 @@
+#include "Rainbow.h"
+#include "WProgram.h"
+
+
+/********************************************
+matrix data format,this is just for reference
+
+0x0grb
+
+data type:unsigned short 16bits.
+every 4bits reperesent one color level, so each color
+is with 16 levels.
+*********************************************/
+
+//used for displaying
+unsigned short matrixColorData[8][8]= // [line][column]
+{
+		{0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff},//wite
+		{0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000},//black
+		{0x000f,0x000d,0x000b,0x0009,0x0007,0x0005,0x0003,0x0001},//blue
+		{0x00ff,0x00dd,0x00bb,0x0099,0x0077,0x0055,0x0033,0x0011},//blue&red
+		{0x00f0,0x00d0,0x00b0,0x0090,0x0070,0x0050,0x0030,0x0010},//red
+                {0x0ff0,0x0dd0,0x0bb0,0x0990,0x0770,0x0750,0x0330,0x0110},//red&green
+		{0x0f00,0x0d00,0x0b00,0x0900,0x0700,0x0500,0x0300,0x0100},//green
+		{0x0f0f,0x0d0d,0x0b0b,0x0909,0x0707,0x0505,0x0303,0x0101},//blue&green
+};
+
+//used for receiving color data from serial port,they can be 
+unsigned short serialColorData[8][8] = 
+{
+		{0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff,0x0fff},//wite
+		{0x05e3,0x05e3,0x05e3,0x05e3,0x05e3,0x05e3,0x05e3,0x05e3},//random
+		{0x000f,0x000f,0x000f,0x000f,0x000f,0x000f,0x000f,0x000f},//blue
+		{0x00ff,0x00ff,0x00ff,0x00ff,0x00ff,0x00ff,0x00ff,0x00ff},//blue&red
+		{0x00f0,0x00f0,0x00f0,0x00f0,0x00f0,0x00f0,0x00f0,0x00f0},//red
+                {0x0ff0,0x0ff0,0x0ff0,0x0ff0,0x0ff0,0x0ff0,0x0ff0,0x0ff0},//red&green
+		{0x0f00,0x0f00,0x0f00,0x0f00,0x0f00,0x0f00,0x0f00,0x0f00},//green
+		{0x0f0f,0x0f0f,0x0f0f,0x0f0f,0x0f0f,0x0f0f,0x0f0f,0x0f0f},//blue&green
+};
+//= //Timer1 interuption service routine=========================================
+ISR(TIMER1_OVF_vect)         
+{
+  static unsigned char line=0,level=0;
+  
+  flash_line(line,level);
+  
+  line++;
+  if(line>7)
+  {
+    line=0;
+    level++;
+    if(level>15)
+    {
+      level=0;
+    }
+  }
+  
+  //wait for serial data, and saved to serialColorData[][]
+  getSerialData();
+  
+}
+
+/**********************************************
+Get led matrix data from serial,
+data format:
+4 bytes a group:--line(1byte),column(1btye),color(2byte)
+color format:--0x0grb,low byte first
+***********************************************/
+void getSerialData()
+{
+  int len = 0;
+  unsigned char line = 0;
+  unsigned char column = 0;
+  unsigned short color = 0;
+  
+  len =Serial.available();
+  
+  if(len >= 4)
+  {
+    //Serial.println(len);
+    line = Serial.read();
+    column = Serial.read();
+    unsigned char low = Serial.read();//low 8bits first
+    unsigned short high = Serial.read();//high 8bits later
+    color = (high<<8)|low;
+    
+    if(line < 8 & line >= 0 & column < 8 & column >= 0)
+    {
+      serialColorData[line][column] = color;
+    }
+  }
+}
+//==============================================================
+
+//shift 1 bit to the rgb data driver MBI5168
+void shift_1_bit(unsigned char LS)
+{
+  if(LS)
+  {
+    shift_data_1;
+  }
+  else
+  {
+    shift_data_0;
+  }
+  clk_rising;
+}
+
+//sweep the specific line,used in the Timer1 ISR
+void flash_line(unsigned char line,unsigned char level)
+{
+  disable_oe;
+  close_all_line;
+  shift_24_bit(line,level);
+  open_line(line);
+  enable_oe;
+}
+
+//one line with 8 rgb data,so totally 8x3=24 bits
+void shift_24_bit(unsigned char line,unsigned char level)
+{
+  unsigned char column=0;
+  unsigned char g=0,r=0,b=0;
+  le_high;
+     //Serial.println(matrixColorData[0][0],HEX);
+    for(column=0;column<8;column++)
+    {
+      g=(matrixColorData[line][column]&0x0fff)>>8;
+	  
+      if(g>level) {shift_1_bit(1);}
+      else {shift_1_bit(0);}  //gray scale,11100000b always light
+      
+    }
+
+    for(column=0;column<8;column++)
+    {
+      r=(matrixColorData[line][column]&0x00FF)>>4;
+      if(level <4)
+      {
+      		if(r>level) {shift_1_bit(1);}
+                else {shift_1_bit(0);}  //gray scale,00011000 always light
+      	}
+	else
+	{
+		if(r>(level-4)) {shift_1_bit(1);}
+                else {shift_1_bit(0);}  //gray scale,00011000 always light
+	}
+      
+    }
+
+    for(column=0;column<8;column++)
+    {
+      b=matrixColorData[line][column]&0x000f;
+	  
+      if(b>level) {shift_1_bit(1);}
+      else {shift_1_bit(0);}  //gray scale,00000111 always light
+      
+    }
+		
+   le_low;
+}
+
+//open the specific line
+void open_line(unsigned char line)
+{
+  switch(line)
+  {
+  case 0:
+    {
+      open_line0;
+      break;
+    }
+  case 1:
+    {
+      open_line1;
+      break;
+    }
+  case 2:
+    {
+      open_line2;
+      break;
+    }
+  case 3:
+    {
+      open_line3;
+      break;
+    }
+  case 4:
+    {
+      open_line4;
+      break;
+    }
+  case 5:
+    {
+      open_line5;
+      break;
+    }
+  case 6:
+    {
+      open_line6;
+      break;
+    }
+  case 7:
+    {
+      open_line7;
+      break;
+    }
+  }
+}
+//======================================================================
+Rainbow::Rainbow(){}
+
+//invoke initIO and initTimer1
+void Rainbow::init(void)
+{
+  initIO();
+  initTimer1();
+}
+
+//initialize IO for controlling the leds
+void Rainbow::initIO(void)
+{
+  DDRD=0xff;//set port D as output
+  DDRC=0xff;//set port C as output
+  DDRB=0xff;//set port B as output
+  PORTD=0;//initialize port D to LOW
+  PORTB=0;//initialize port B to LOW
+}
+
+//initialize Timer1 to 100us overflow           
+void Rainbow::initTimer1(void)        
+{
+  TCCR1A = 0;                 // clear control register A
+  TCCR1B = _BV(WGM13);        // set mode as phase and frequency correct pwm, stop the timer
+  ICR1=800;                   //(XTAL * microseconds) / 2000000  100us cycles 
+  TIMSK1 = _BV(TOIE1);
+  TCNT1 = 0;
+  TCCR1B |= _BV(CS10);
+  sei();                      //enable global interrupt
+}
+
+//close all leds
+void Rainbow::closeAll()
+{
+  for(int i = 0; i < 8; i++)
+    for(int j = 0; j < 8; j++)
+      matrixColorData[i][j] = 0;
+}
+
+//close one line
+void Rainbow::closeOneLine(unsigned char line)
+{
+  for(int j = 0; j < 8; j++)
+    matrixColorData[line][j] = 0;
+}
+
+//close one column
+void Rainbow::closeOneColumn(unsigned char column)
+{
+  for(int i = 0; i < 8; i++)
+    matrixColorData[i][column] = 0;
+}
+
+//close specific dot
+void Rainbow::closeOneDot(unsigned char line, unsigned char column)
+{
+  matrixColorData[line][column] = 0;
+}
+
+//close one diagonal line
+void Rainbow::closeOneDiagonal(unsigned char line, unsigned char type)
+{
+  int num = 0;//number of lighting leds     
+  
+  if(LEFT_BOTTOM_TO_RIGHT_TOP == type)  
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][k+8-num] = 0;
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-k-1][k] = 0;
+     }
+    }
+  }
+  else if(LEFT_TOP_TO_RIGHT_BOTTOM == type)
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-1-k][7-k] = 0;
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][num-1-k] = 0;
+     }
+    }
+  }
+}
+  
+  
+//light all with one color
+void Rainbow::lightAll(unsigned short colorData)
+{
+  for(int i = 0; i < 8; i++)
+    for(int j = 0; j < 8; j++)
+      matrixColorData[i][j] = colorData;
+}
+
+//light all with matrix data
+void Rainbow::lightAll(unsigned short colorData[8][8])
+{
+  for(int i = 0; i < 8; i++)
+    for(int j = 0; j < 8; j++)
+      matrixColorData[i][j] = colorData[i][j];
+}
+
+//only light one line with one color
+void Rainbow::lightOneLine(unsigned char line, unsigned short color,unsigned char othersState)
+{
+  if( OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+  for(int k = 0; k < 8; k++)
+    matrixColorData[line][k] = color;
+}
+//only light one line with 8 colors
+void Rainbow::lightOneLine(unsigned char line, unsigned short color[8],unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+  
+  for(int k = 0; k < 8; k++)
+    matrixColorData[line][k] = color[k];
+}
+
+//only light one column with one color
+void Rainbow::lightOneColumn(unsigned char column, unsigned short color,unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+        
+  for(int k = 0; k < 8; k++)
+    matrixColorData[k][column] = color;  
+}
+
+//only light one column with 8 colors
+void Rainbow::lightOneColumn(unsigned char column, unsigned short color[8],unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+        
+  for(int k = 0; k < 8; k++)
+    matrixColorData[k][column] = color[k];  
+
+}
+
+//only light one column with serialColorData 8 colors
+void Rainbow::lightOneColumn(unsigned char column, unsigned short color[8][8],unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+        
+  for(int k = 0; k < 8; k++)
+    matrixColorData[k][column] = color[k][column];  
+}
+
+//only light one dot at specific position
+void Rainbow::lightOneDot(unsigned char line,unsigned char column, unsigned short color,unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+        
+  matrixColorData[line][column] = color;
+}
+
+//only light one diagonal line  with one color
+void Rainbow::lightOneDiagonal(unsigned char line, unsigned char type, unsigned short color,unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+  
+  int num = 0;//number of lighting leds     
+  
+  if(LEFT_BOTTOM_TO_RIGHT_TOP == type)  
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][k+8-num] = color;
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-k-1][k] = color;
+     }
+    }
+  }
+  else if(LEFT_TOP_TO_RIGHT_BOTTOM == type)
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-1-k][7-k] = color;
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][num-1-k] = color;
+     }
+    }
+  }
+}
+
+//only light one diagonal line with a number of colors
+void Rainbow::lightOneDiagonal(unsigned char line, unsigned char type, unsigned short *color,unsigned char othersState)
+{
+  if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+  
+  int num = 0;//number of lighting leds     
+  
+  if(LEFT_BOTTOM_TO_RIGHT_TOP == type)  
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][k+8-num] = color[k];
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-k-1][k] = color[k];
+     }
+    }
+  }
+  else if(LEFT_TOP_TO_RIGHT_BOTTOM == type)
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-1-k][7-k] = color[k];
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][num-1-k] = color[k];
+     }
+    }
+  }
+
+}
+
+//only light one diagonal line with serialColorData colors
+void Rainbow::lightOneDiagonal(unsigned char line, unsigned char type, unsigned short color[8][8],unsigned char othersState)
+{
+    if(OTHERS_OFF == othersState)
+  {
+    for(int i = 0; i < 8; i++)
+      for(int j = 0; j < 8; j++)    
+          matrixColorData[i][j] = 0;
+  }
+  
+  int num = 0;//number of lighting leds     
+  
+  if(LEFT_BOTTOM_TO_RIGHT_TOP == type)  
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][k+8-num] = color[7-k][k+8-num];
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-k-1][k] = color[num-k-1][k];
+     }
+    }
+  }
+  else if(LEFT_TOP_TO_RIGHT_BOTTOM == type)
+  {
+    if(line&0x08)//line = 8...15
+    {
+      num = 15 - line;
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[num-1-k][7-k] = color[num-1-k][7-k];
+     }
+    }
+    else//line = 0...7
+    {
+      num = line + 1;
+      
+     for(int k = 0; k < num; k++)
+     {
+        matrixColorData[7-k][num-1-k] = color[7-k][num-1-k];
+     }
+    }
+  }
+
+}
+
+
+
---
+++ trunk/Rainbowduino_v1_0_4/Rainbow.h
@@ -0,0 +1,116 @@
+#ifndef Rainbow_h
+#define Rainbow_h
+
+//reperesents the 8x8 matrix color data,so just change the coresponding color data
+extern unsigned short matrixColorData[8][8];//defined in data.c
+
+//for receive color data from serial port
+extern unsigned short serialColorData[8][8];//defined in data.c
+
+//=============================================
+//MBI5168
+#define SH_DIR_OE    DDRC
+#define SH_DIR_SDI   DDRC
+#define SH_DIR_CLK   DDRC
+#define SH_DIR_LE    DDRC
+
+#define SH_BIT_OE    0x08
+#define SH_BIT_SDI   0x01
+#define SH_BIT_CLK   0x02
+#define SH_BIT_LE    0x04
+
+#define SH_PORT_OE   PORTC
+#define SH_PORT_SDI  PORTC
+#define SH_PORT_CLK  PORTC
+#define SH_PORT_LE   PORTC
+//============================================
+#define clk_rising  {SH_PORT_CLK&=~SH_BIT_CLK;SH_PORT_CLK|=SH_BIT_CLK;}
+#define le_high     {SH_PORT_LE|=SH_BIT_LE;}
+#define le_low      {SH_PORT_LE&=~SH_BIT_LE;}
+#define enable_oe   {SH_PORT_OE&=~SH_BIT_OE;}
+#define disable_oe  {SH_PORT_OE|=SH_BIT_OE;}
+
+#define shift_data_1     {SH_PORT_SDI|=SH_BIT_SDI;}
+#define shift_data_0     {SH_PORT_SDI&=~SH_BIT_SDI;}
+//============================================
+#define open_line0	{PORTB=0x04;}
+#define open_line1	{PORTB=0x02;}
+#define open_line2	{PORTB=0x01;}
+#define open_line3	{PORTD=0x80;}
+#define open_line4	{PORTD=0x40;}
+#define open_line5	{PORTD=0x20;}
+#define open_line6	{PORTD=0x10;}
+#define open_line7	{PORTD=0x08;}
+#define close_all_line	{PORTD&=~0xf8;PORTB&=~0x07;}
+//============================================
+
+//Get led matrix data from serial
+void getSerialData();
+
+//shift 1 bit to the rgb data driver MBI5168
+void shift_1_bit(unsigned char LS);
+
+//sweep the specific line,used in the Timer1 ISR
+void flash_line(unsigned char line,unsigned char level);
+
+//one line with 8 rgb data,so totally 8x3=24 bits
+void shift_24_bit(unsigned char line,unsigned char level);
+
+//open the specific line
+void open_line(unsigned char line);
+//===============================================
+//0x0grb
+#define RED 0x00f0
+#define GREEN 0x0f00
+#define BLUE 0x000f
+#define BLACK 0x0000
+#define WHITE 0x0fff
+#define VIOLET 0x00ff
+#define YELLOW 0x0ff0
+#define AQUA  0x0f0f
+#define RANDOM 0x5e3
+
+//diagonal type
+#define  LEFT_BOTTOM_TO_RIGHT_TOP 0
+#define  LEFT_TOP_TO_RIGHT_BOTTOM 1
+
+//when change leds,other led state 
+#define OTHERS_ON 1
+#define OTHERS_OFF 0
+
+class Rainbow
+{
+  public:
+  Rainbow(void);
+ 
+  void init(void);//invoke initIO and initTimer1
+  void initIO(void);//initialize IO for controlling the leds            
+  void initTimer1(void);//initialize Timer1 to 100us overflow 
+  
+  void closeAll();//close all leds
+  void closeOneLine(unsigned char line);//close one line
+  void closeOneColumn(unsigned char column);//close one column
+  void closeOneDot(unsigned char line, unsigned char column);//close specific dot
+  void closeOneDiagonal(unsigned char line, unsigned char type);//close one diagonal line
+  
+  void lightAll(unsigned short colorData);//light all with one color
+  void lightAll(unsigned short colorData[8][8]);//light all with matrix data
+  
+  void lightOneLine(unsigned char line, unsigned short color,unsigned char othersState);//only light one line with one color
+  void lightOneLine(unsigned char line, unsigned short color[8],unsigned char othersState);//only light one line with 8 colors
+  
+  void lightOneColumn(unsigned char column, unsigned short color,unsigned char othersState);//only light one column with one color
+  void lightOneColumn(unsigned char column, unsigned short color[8],unsigned char othersState);//only light one column with 8 colors
+  void lightOneColumn(unsigned char column, unsigned short color[8][8],unsigned char othersState);//only light one column with serialColorData 8 colors
+  
+  void lightOneDot(unsigned char line, unsigned char column, unsigned short color,unsigned char othersState);//only light one dot at specific position
+  
+  void lightOneDiagonal(unsigned char line, unsigned char type, unsigned short color,unsigned char othersState);//only light one diagonal line  with one color
+  void lightOneDiagonal(unsigned char line, unsigned char type, unsigned short *color,unsigned char othersState);//only light one diagonal line with a number of colors
+  void lightOneDiagonal(unsigned char line, unsigned char type, unsigned short color[8][8],unsigned char othersState);//only light one diagonal line with serialColorData colors
+  
+  //void demoFlash(unsigned short *color);//demo flash
+  //void lightNumber(int num,unsigned short color);//light 0~9
+};
+
+#endif
